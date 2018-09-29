4c6c539cad52c1b908302b63e8139cf0d240fa8c
shradhakatyal <skatyal2204@gmail.com>
2018-09-29 23:13:08 +0530

    default places and ViewModel added

--- js/app.js
+++ js/app.js
@@ -1,16 +1,96 @@
-// Initializing the Map
-var mymap = L.map('my-map').setView([28.6041, 77.1025], 12);
+var viewModel, place;
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1Ijoic2hyYWRoYWthdHlhbCIsImEiOiJjam1kbHJzeWs2YTYzM3BwNDZvaTQ4N3ltIn0.8MFs3lWffgKywgmrM3hymA'
-}).addTo(mymap);
+// A list of places added by default to the map
 
-// Adding default markers to the map
-var marker = L.marker([28.5426, 77.1831]).addTo(mymap);
-var marker = L.marker([28.5562, 77.1000]).addTo(mymap);
-var marker = L.marker([28.5244, 77.1855]).addTo(mymap);
-var marker = L.marker([28.5932, 77.2506]).addTo(mymap);
-var marker = L.marker([28.6562, 77.2410]).addTo(mymap);
+var places = [
+{
+    name: "India Gate", 
+    location:{
+        "lat": 28.6129,
+        'lng': 77.2295
+    }
+},
+{
+    name: "Lotus Temple",
+    location: {
+        "lat": 28.5535,
+        "lng": 77.2588
+    }
+},
+{
+    name: "Qutub Minar",
+    location: {
+        "lat": 28.5244,
+        'lng': 77.1855
+    }
+},
+{
+    name: "Red Fort", 
+    location: {
+        "lat": 28.6562,
+        "lng": 77.2410
+    }
+},
+{
+    name: "Humayun's Tomb",
+    location: {
+        "lat": 28.5932,
+        "lng": 77.2506
+    }
+},
+{
+    name: "Lodi Gardens", 
+    location: {
+        "lat": 28.5931,
+        "lng": 77.2197
+    }
+}
+]
+
+// Initialising the map
+function initMap() {
+    var center = {lat: 28.6315, lng: 77.2167}
+    var map = new google.maps.Map(
+        document.getElementById('my-map-js'), {zoom: 12, center: center});
+    
+    // Populating the markers onto the map
+    for(var index = 0;index<places.length;index++) {
+        var place = places[index];
+        var marker = new google.maps.Marker({
+            position: place["location"],
+            map: map,
+            title: place["name"],
+            animation: google.maps.Animation.DROP
+        });
+    }
+}
+
+function createLocationArray(places) {
+    var temp = [];
+    for(var i=0;i<places.length;i++) {
+        var obj = {"name": places[i].name}
+        temp.push(obj);
+    }
+    return temp;
+}
+
+function ViewModel() {
+    var self = this;
+    var place = createLocationArray(places)
+    self.place = ko.observableArray(place);
+    self.searchQuery = ko.observable('');
+
+    self.filterPlaces = function() {
+        var filter = self.searchQuery().toLowerCase();
+        place = self.place().filter(function(p) {
+            return p.name.toLowerCase().indexOf(filter) > -1
+        });
+        console.log(place);
+    }
+}
+
+// activate knockout apply binding
+ko.applyBindings(new ViewModel());
+
+
+
