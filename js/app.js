ed033458613c4f4f3e15b9db1774ecd6b1131101
shradhakatyal <skatyal2204@gmail.com>
2018-10-06 00:23:53 +0530

    foursquare api integrated

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 var viewModel, place, map;
 var markers = [];
-
+var CLIENT_ID = "0RPZZONT1T0CSYPX2KCESXOG5SPAXE3BOO34RKTJY1HQY2ZI";
+var CLIENT_SECRET = "NAZQIKCE5AQC0GSYB2KVJ3KJFNDMJCTMZLTC1G3Y3YPVQWET";
 // A list of places added by default to the map
 
 var places = [
 {
-    name: "India Gate", 
+    name: "India Gate",
     location:{
         "lat": 28.6129,
         'lng': 77.2295
@@ -26,7 +27,7 @@ var places = [
     }
 },
 {
-    name: "Red Fort", 
+    name: "Red Fort",
     location: {
         "lat": 28.6562,
         "lng": 77.2410
@@ -40,7 +41,7 @@ var places = [
     }
 },
 {
-    name: "Lodi Gardens", 
+    name: "Lodi Gardens",
     location: {
         "lat": 28.5931,
         "lng": 77.2197
@@ -52,50 +53,119 @@ var places = [
 function initMap() {
     var center = {lat: 28.6315, lng: 77.2167}
     map = new google.maps.Map(
-        document.getElementById('my-map-js'), {zoom: 12, center: center});
-    
+        document.getElementById('my-map-js'), {zoom: 11, center: center});
+
     // Populating the markers onto the map
     for(var index = 0;index<places.length;index++) {
-        var place = places[index];
-        var marker = new google.maps.Marker({
-            position: place["location"],
-            map: map,
-            title: place["name"],
-            animation: google.maps.Animation.DROP
-        });
-        // Storing all the markers in an array.
-        markers.push(marker);
-        
-        // Assigning marker property to place array.
-        viewModel.place()[index].marker = marker;
+        // Using IIFE to add markers and adding click event listener to each marker.
+        (function() {
+            var place = places[index];
+            var marker = new google.maps.Marker({
+                position: place["location"],
+                map: map,
+                title: place["name"],
+                animation: google.maps.Animation.DROP
+            });
 
-        var infoWindow = new google.maps.InfoWindow();
-        // Click listener to open info window when a marker is clicked.
-        marker.addListener('click', function() {
-            setInfoWindow(this, infoWindow);
-            // infoWindow.setContent(contentString);
-        });
+            // Storing all the markers in an array.
+            markers.push(marker);
+
+            // Assigning marker property to place array.
+            viewModel.place()[index].marker = marker;
+
+            var infoWindow = new google.maps.InfoWindow();
+
+            // Click listener to open info window when a marker is clicked.
+            marker.addListener('click', function() {
+                setInfoWindow(this, infoWindow);
+                infoWindow.setContent(contentString);
+            });
+
+            // Function to set the info window
+            function setInfoWindow(marker, infoWindow) {
+                if (infoWindow.marker != marker) {
+                    infoWindow.marker = marker;
+                    infoWindow.setContent("<div class='title'>" + marker.title + "</div>" + marker.contentString);
+                }
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function() {
+                    marker.setAnimation(null);
+                }, 500);
+                infoWindow.open(map, marker);
+                
+                infoWindow.addListener('closeclick', function() {
+                    infoWindow.setMarker = null;
+                });
+
+                //Automatically close info window after 3 seconds
+                // window.setTimeout(function(){
+                //     infoWindow.close();
+                // }, 3000);
+            }
+
+            // Fetching data from foursquare api for each marker
+
+            // foursquare api url
+            var foursquareUrl = "https://api.foursquare.com/v2/venues/search";
+            var contentString;
+            var venue, address, category, foursquareId;
+            // ajax request - foursquare api data (https://developer.foursquare.com/docs/)
+            $.ajax({
+                url: foursquareUrl,
+                dataType: "json",
+                data: {
+                    client_id: CLIENT_ID,
+                    client_secret: CLIENT_SECRET,
+                    query:  marker.title, // gets data from marker.title (array of object)
+                    near: "Delhi",
+                    v: 20181005 // version equals date
+                },
+                success: function(data) {
+                    // console.log(data);
+                        // get venue info
+                        venue = data.response.venues[0];
+                        console.log(venue)
+                        address = venue.location.formattedAddress;
+                        category = venue.categories[0].name;
+                        // gets link of place
+                        foursquareId = "https://foursquare.com/v/" + venue.id;
+                        
+                        photoUrl = "https://api.foursquare.com/v2/venues/"+venue.id+"/photos";
+                        
+                        // populates infowindow with api info
+                        contentString = "<div class='name'>" + "Name: " + "<span class='info'>" + venue.name + "</span></div>" +
+                            "<div class='category'>" + "Catergory: " + "<span class='info'>" + category + "</span></div>" +
+                            "<div class='address'>" + "Location: " + "<span class='info'>" + address + "</span></div>" +
+                            "<div class='information'>" + "More info: " + "<a href='" + foursquareId + "'>" + "Click here" + "</a></div>";
+                            getPhoto(photoUrl);
+                            marker.contentString;
+                },
+                error: function() {
+                    contentString = "<div class='error'>Something went wrong. Please try again later.</div>"
+                }
+            });
+
+            function getPhoto(url) {
+                $.ajax({
+                    url: url,
+                    data: {
+                        client_id: CLIENT_ID,
+                        client_secret: CLIENT_SECRET,
+                        v: 20181005
+                    },
+                    success: function(data) {
+                        console.log(data);
+                        var imgUrl = data.response.photos.items[0].prefix+'100x100'+data.response.photos.items[0].suffix;
+                        contentString = "<div class='image'><img src='"+imgUrl+"'></div>"+contentString;
+                    },
+                    error: function(err) {
+                        console.log(err);
+                    }
+                });
+            }
+        })();
     }
 }
-
-// Function to set the info window
-function setInfoWindow(marker, infoWindow, title) {
-    // Check if the marker is closed or not.
-    if (infoWindow.marker != marker) {
-        infoWindow.marker = marker;
-        infoWindow.setContent('<div class="title">' + marker.title + '</div>');
-
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function() {
-            marker.setAnimation(null);
-        }, 500);
-        infoWindow.open(map, marker);
-        infoWindow.addListener('closeclick', function() {
-            infoWindow.setMarker = null;
-        });
-    }
-}
-
 
 // Create Location object
 function createLocation(location) {
@@ -133,12 +203,11 @@ function ViewModel() {
                 }
             }
         }
-    })
-
-    self.place.subscribe(function(place) {
-        console.log('here')
-        self.filterPlaces(place);
     });
+
+    self.showLocationOnMap = function(locations) {
+        google.maps.event.trigger(locations.marker, 'click');
+    };
 }
 
 var viewModel = new ViewModel()
@@ -149,8 +218,9 @@ ko.applyBindings(viewModel);
 function toggleMenu(el) {
     if(screen.width <= 768) {
         var listView = document.querySelector('.list-view');
-        el.classList.toggle('change');
-        console.log(listView.style.display);
+        var menuContainer = document.querySelector('.menu-container');
+
+        menuContainer.classList.toggle('change');
         if(window.getComputedStyle(listView).display === 'none') {
             listView.style.display = 'block';
         } else if(window.getComputedStyle(listView).display === 'block') {
